13a0f9ce6fc58cf7a41cc60cf8c1ede88cc58d12
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2025-12-04 13:41:30 +1100

    Saving files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from imap_tools import MailBox
 from tkinter import filedialog
 import tkinter as tk
+import os
 
 def GetFolderInteractive() -> str:
     root = tk.Tk()
@@ -11,7 +12,7 @@ def GetFolderInteractive() -> str:
     root.destroy() 
     return folder_path
 
-def FindEmailWithUsername(username:str, passcode:str, checkUserName:str, depth:int=None, reverse:bool=True):
+def FindEmailWithUsername(username:str, passcode:str, checkUserName:str, *, depth:int=None, reverse:bool=True):
     foundMessages = []
     
     with MailBox('imap.gmail.com').login(username, passcode) as mailbox:
@@ -30,7 +31,28 @@ def GetUserInformation() -> tuple[str, str, str]:
     
     return username, passcode, checkUserName
 
+def CreateTXTFile(location:str, name:str, text:str) -> str:
+    try:
+        fileLocation = os.path.join(location, SantiseNameForFile(name) + ".txt")
+        
+        if not os.path.exists(fileLocation):
+            with open(fileLocation, "w", encoding="utf-8") as newFile:
+                newFile.write(text)
+            return fileLocation  
+        else:
+            print("A file by that name already exists")
+            return None
+    except Exception as err:
+        print(err)
+        return None
+
+def SantiseNameForFile(name:str) -> str:
+    return name.replace(":", "-").replace("/", "-").replace("\\", "-")
+
 folder = GetFolderInteractive()
 
-print(FindEmailWithUsername(*GetUserInformation()))
+messages = FindEmailWithUsername(*GetUserInformation(),depth=20)
 
+for message in messages:
+    name =  message.date_str + " " + message.from_
+    print(CreateTXTFile(folder, name, message.text or message.html))
