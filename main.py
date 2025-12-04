0b82dc1fae0ef70adbf5cd220fd0b219e1514260
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2025-12-04 12:21:22 +1100

    Get Folder Interactiive

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from imap_tools import MailBox
+from tkinter import filedialog
+import tkinter as tk
+
+def GetFolderInteractive() -> str:
+    root = tk.Tk()
+    root.withdraw()
+    
+    folder_path = filedialog.askdirectory(title="Select a folder for files to be saved")
+    
+    root.destroy() 
+    return folder_path
 
 def FindEmailWithUsername(username:str, passcode:str, checkUserName:str, depth:int=None, reverse:bool=True):
+    foundMessages = []
+    
     with MailBox('imap.gmail.com').login(username, passcode) as mailbox:
         for msg in mailbox.fetch(reverse=reverse, limit=depth):
             if  msg.from_ == checkUserName:
-                print(msg.from_, msg.subject)
+                foundMessages.append(msg)
+    
+    return foundMessages
 
 def GetUserInformation() -> tuple[str, str, str]:
     username = input("username: ")
@@ -15,5 +30,7 @@ def GetUserInformation() -> tuple[str, str, str]:
     
     return username, passcode, checkUserName
 
-FindEmailWithUsername(*GetUserInformation())
+folder = GetFolderInteractive()
+
+print(FindEmailWithUsername(*GetUserInformation()))
 
